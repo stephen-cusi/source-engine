b50f768459c9c3c9a0310c2a85380cc047fbee0f
klaymen1n <lastketchup129@gmail.com>
2023-03-17 00:58:38 +0300

    modified:   scripts/build-android-aarch64.sh 	modified:   scripts/build-android-armv7a.sh

--- scripts/build-android-aarch64.sh
+++ scripts/build-android-aarch64.sh
@@ -5,5 +5,8 @@ wget https://dl.google.com/android/repository/android-ndk-r10e-linux-x86_64.zip 
 unzip android-ndk-r10e-linux-x86_64.zip
 export ANDROID_NDK_HOME=$PWD/android-ndk-r10e/
 export NDK_HOME=$PWD/android-ndk-r10e/
-./waf configure -T debug --android=aarch64,4.9,21 -8 --togles --disable-warns &&
-./waf build
+wget https://github.com/llvm/llvm-project/releases/download/llvmorg-11.1.0/clang+llvm-11.1.0-x86_64-linux-gnu-ubuntu-16.04.tar.xz -o /dev/null
+tar -xf clang+llvm-11.1.0-x86_64-linux-gnu-ubuntu-16.04.tar.xz
+export PATH="$PWD/clang+llvm-11.1.0-x86_64-linux-gnu-ubuntu-16.04/bin:$PATH"
+./waf configure -T release --android=aarch64,4.9,21 -8 --togles --disable-warns &&
+./waf install --target=client,server
