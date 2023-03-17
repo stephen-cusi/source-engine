61e342b91f3e6676012ff6505d752a1771e8b9e8
klaymen1n <lastketchup129@gmail.com>
2023-03-17 04:48:33 +0300

    modified:   .github/workflows/build.yml 	modified:   scripts/build-android-aarch64.sh 	modified:   scripts/build-android-armv7a.sh

--- scripts/build-android-aarch64.sh
+++ scripts/build-android-aarch64.sh
@@ -3,5 +3,8 @@
 git submodule init && git submodule update
 wget https://dl.google.com/android/repository/android-ndk-r10e-linux-x86_64.zip -o /dev/null
 unzip android-ndk-r10e-linux-x86_64.zip
+wget https://github.com/llvm/llvm-project/releases/download/llvmorg-11.1.0/clang+llvm-11.1.0-x86_64-linux-gnu-ubuntu-16.04.tar.xz -o /dev/null
+tar -xf clang+llvm-11.1.0-x86_64-linux-gnu-ubuntu-16.04.tar.xz
+export PATH="$PWD/clang+llvm-11.1.0-x86_64-linux-gnu-ubuntu-16.04/bin:$PATH"
 ./waf configure -T release --build-game=hl2sb --android=aarch64,host,21 --target=../aarch64 -8 --togles --disable-warns &&
 ./waf install --target=client,server
