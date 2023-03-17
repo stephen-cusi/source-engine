dbf8638028f9da5a6337daec18c9f5d736b2effc
klaymen1n <lastketchup129@gmail.com>
2023-03-17 04:54:08 +0300

    modified:   scripts/build-android-aarch64.sh

--- scripts/build-android-aarch64.sh
+++ scripts/build-android-aarch64.sh
@@ -3,6 +3,7 @@
 git submodule init && git submodule update
 wget https://dl.google.com/android/repository/android-ndk-r10e-linux-x86_64.zip -o /dev/null
 unzip android-ndk-r10e-linux-x86_64.zip
+export ANDROID_NDK_HOME=$PWD/android-ndk-r10e/
 wget https://github.com/llvm/llvm-project/releases/download/llvmorg-11.1.0/clang+llvm-11.1.0-x86_64-linux-gnu-ubuntu-16.04.tar.xz -o /dev/null
 tar -xf clang+llvm-11.1.0-x86_64-linux-gnu-ubuntu-16.04.tar.xz
 export PATH="$PWD/clang+llvm-11.1.0-x86_64-linux-gnu-ubuntu-16.04/bin:$PATH"
