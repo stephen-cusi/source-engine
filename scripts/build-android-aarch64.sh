bc9fbc87f813d132451a3627444e32a4bb946473
steadyfield <steadyfield111@gmail.com>
2023-09-12 13:57:24 +0800

    Update build-android-aarch64.sh

--- scripts/build-android-aarch64.sh
+++ scripts/build-android-aarch64.sh
@@ -2,4 +2,4 @@
 export ANDROID_NDK_HOME=$PWD/android-ndk-r10e/
 export PATH="$PWD/clang+llvm-11.1.0-x86_64-linux-gnu-ubuntu-16.04/bin:$PATH"
 ./waf configure -T release --build-game=hl2sb --prefix=modlauncher-waf/android --android=aarch64,host,21 --target=../aarch64 -8 --disable-warns &&
-./waf install --target=client,server --strip
+./waf install --target=client,server,engine --strip
