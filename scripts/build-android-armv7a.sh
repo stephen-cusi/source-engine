f4e26c47ccd9073b793c2a9acf3a4424ea7dbabd
steadyfield <steadyfield111@gmail.com>
2023-09-12 13:57:08 +0800

    Update build-android-armv7a.sh

--- scripts/build-android-armv7a.sh
+++ scripts/build-android-armv7a.sh
@@ -7,4 +7,4 @@ wget https://github.com/llvm/llvm-project/releases/download/llvmorg-11.1.0/clang
 tar -xf clang+llvm-11.1.0-x86_64-linux-gnu-ubuntu-16.04.tar.xz
 export PATH="$PWD/clang+llvm-11.1.0-x86_64-linux-gnu-ubuntu-16.04/bin:$PATH"
 ./waf configure -T release --build-game=hl2sb --prefix=modlauncher-waf/android --android=armeabi-v7a-hard,host,21 --target=../armeabi-v7a --disable-warns &&
-./waf install --target=client,server --strip
+./waf install --target=client,server,engine --strip
