81d3a1b3a1ea3487b611062a01e55fcdf868c04b
stephen-cusi <111830109+stephen-cusi@users.noreply.github.com>
2023-10-06 23:45:08 +0800

    [HL2MP] Fix RPG laser sprite precache warning
    https://github.com/HL2RP/HL2RP/commit/ff2edf4fdeef572c27439068dc599609daa4d4e9

--- game/shared/hl2mp/weapon_rpg.cpp
+++ game/shared/hl2mp/weapon_rpg.cpp
@@ -1345,7 +1345,7 @@ void CAPCMissile::ComputeActualDotPosition(CLaserDot *pLaserDot, Vector *pActual
 
 #define	RPG_BEAM_SPRITE		"effects/laser1.vmt"
 #define	RPG_BEAM_SPRITE_NOZ	"effects/laser1_noz.vmt"
-#define	RPG_LASER_SPRITE	"sprites/redglow1"
+#define	RPG_LASER_SPRITE	"sprites/redglow1.vmt"
 
 //=============================================================================
 // RPG
