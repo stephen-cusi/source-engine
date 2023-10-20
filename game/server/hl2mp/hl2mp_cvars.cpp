e87d3574a6cb9707be891e779b17cc2818b744ff
stephen-cusi <magiskcurry@qq.com>
2023-10-20 13:13:18 +0800

    Fix compilation errors in stupid

--- game/server/hl2mp/hl2mp_cvars.cpp
+++ game/server/hl2mp/hl2mp_cvars.cpp
@@ -12,8 +12,8 @@
 
 // Ready restart
 ConVar mp_readyrestart(
-							"mp_readyrestart", 
-							"0", 
+							"mp_readyrestart",
+							"0",
 							FCVAR_GAMEDLL,
 							"If non-zero, game will restart once each player gives the ready signal" );
 
@@ -23,3 +23,17 @@ ConVar mp_ready_signal(
 							"ready",
 							FCVAR_GAMEDLL,
 							"Text that each player must speak for the match to begin" );
+
+// Skip defaults
+ConVar mp_skipdefaults(
+                            "mp_skipdefaults",
+                            "0",
+                            FCVAR_GAMEDLL,
+                            "If non-zero, game will not give player default weapons and ammo" );
+
+// Allow pickup items with +use
+ConVar mp_allowpickup(
+                            "mp_allowpickup",
+                            "1",
+                            FCVAR_GAMEDLL,
+                            "If non-zero, game will allow pickup items" );
