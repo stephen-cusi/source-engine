b135f95bd50038591b5e376401d327b1df6d4041
stephen-cusi <magiskcurry@qq.com>
2023-11-05 01:38:11 +0800

    successfully fix build

--- scripts/build-android-armv7a.sh
+++ scripts/build-android-armv7a.sh
@@ -7,4 +7,5 @@ wget https://github.com/llvm/llvm-project/releases/download/llvmorg-11.1.0/clang
 tar -xf clang+llvm-11.1.0-x86_64-linux-gnu-ubuntu-16.04.tar.xz
 export PATH="$PWD/clang+llvm-11.1.0-x86_64-linux-gnu-ubuntu-16.04/bin:$PATH"
 ./waf configure -T release --build-game=hl2sb --prefix=modlauncher-waf/android --android=armeabi-v7a-hard,host,21 --target=../armeabi-v7a --disable-warns &&
-./waf install --target=client,server,engine --strip
+./waf install --target=client,server --strip
+# 加引擎脑子纯粹喀了屎
