bcbc107d806987c3e17353d254e9d1cb1d86055d
stephen-cusi <magiskcurry@qq.com>
2023-11-19 14:31:35 +0800

    repair path

--- scripts/build-android-armv7a.sh
+++ scripts/build-android-armv7a.sh
@@ -9,7 +9,7 @@ export PATH="$PWD/clang+llvm-11.1.0-x86_64-linux-gnu-ubuntu-16.04/bin:$PATH"
 ./waf configure -T release --build-game=hl2sb --prefix=modlauncher-waf/android --android=armeabi-v7a-hard,host,21 --target=../armeabi-v7a --disable-warns &&
 ./waf install --target=client,server --strip
 # 移除多余lib
-rm modlauncher-waf/android/lib/libtier0.so
-rm modlauncher-waf/android/lib/libsteam_api.so
-rm modlauncher-waf/android/lib/libvstdlib.so
+rm modlauncher-waf/android/lib/armeabi-v7a/libtier0.so
+rm modlauncher-waf/android/lib/armeabi-v7a/libsteam_api.so
+rm modlauncher-waf/android/lib/armeabi-v7a/libvstdlib.so
 # 加引擎脑子纯粹喀了屎
