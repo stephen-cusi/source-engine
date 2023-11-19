22edd240b0d951740484bfb518ad4f6f25a0d105
stephen-cusi <magiskcurry@qq.com>
2023-11-19 15:07:18 +0800

    I'm a fool

--- scripts/build-android-aarch64.sh
+++ scripts/build-android-aarch64.sh
@@ -4,7 +4,7 @@ export PATH="$PWD/clang+llvm-11.1.0-x86_64-linux-gnu-ubuntu-16.04/bin:$PATH"
 ./waf configure -T release --build-game=hl2sb --prefix=modlauncher-waf/android --android=aarch64,host,21 --target=../aarch64 -8 --disable-warns &&
 ./waf install --target=client,server --strip
 # 移除多余lib
-rm modlauncher-waf/android/lib/aarch64/libtier0.so
-rm modlauncher-waf/android/lib/aarch64/libsteam_api.so
-rm modlauncher-waf/android/lib/aarch64/libvstdlib.so
+rm modlauncher-waf/android/lib/arm64-v8a/libtier0.so
+rm modlauncher-waf/android/lib/arm64-v8a/libsteam_api.so
+rm modlauncher-waf/android/lib/arm64-v8a/libvstdlib.so
 # 加引擎脑子纯粹喀了屎
