5adeee447d914ee27e81306371707bcd7a8fdfd0
stephen-cusi <magiskcurry@qq.com>
2023-11-19 15:36:37 +0800

    Add vpc and update scripts

--- scripts/build-android-armv7a.sh
+++ scripts/build-android-armv7a.sh
@@ -6,7 +6,7 @@ export ANDROID_NDK_HOME=$PWD/android-ndk-r10e/
 wget https://github.com/llvm/llvm-project/releases/download/llvmorg-11.1.0/clang+llvm-11.1.0-x86_64-linux-gnu-ubuntu-16.04.tar.xz -o /dev/null
 tar -xf clang+llvm-11.1.0-x86_64-linux-gnu-ubuntu-16.04.tar.xz
 export PATH="$PWD/clang+llvm-11.1.0-x86_64-linux-gnu-ubuntu-16.04/bin:$PATH"
-./waf configure -T release --prefix=modlauncher-waf/android --android=armeabi-v7a-hard,host,21 --target=../armeabi-v7a --disable-warns -4 &&
+./waf configure -T release --prefix=modlauncher-waf/android --android=armeabi-v7a-hard,host,21 --target=../armeabi-v7a --disable-warns -4 --build-game=lmod &&
 ./waf install --target=client,server --strip
 # 移除多余lib
 rm modlauncher-waf/android/lib/armeabi-v7a/libtier0.so
