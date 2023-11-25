a5c494f43cd5ffd96a933ffc339424ce137a65eb
stephen-cusi <magiskcurry@qq.com>
2023-11-26 02:05:12 +0800

    add NH2 code

--- game/server/triggers.cpp
+++ game/server/triggers.cpp
@@ -984,6 +984,9 @@ void CTriggerOnce::Spawn( void )
 // ##################################################################################
 #define SF_TRIGGERLOOK_FIREONCE		128
 #define SF_TRIGGERLOOK_USEVELOCITY	256
+#ifdef NH2
+#define SF_TRIGGERLOOK_INVERT		512
+#endif
 
 class CTriggerLook : public CTriggerOnce
 {
@@ -1162,12 +1165,25 @@ void CTriggerLook::Touch(CBaseEntity *pOther)
 
 			if (m_flLookTimeTotal >= m_flLookTime)
 			{
+#ifdef NH2
+				if( !HasSpawnFlags(SF_TRIGGERLOOK_INVERT) )
+				{
+					Trigger(pOther,false);
+				}
+#else
 				Trigger(pOther, false);
+#endif
 			}
 		}
 		else
 		{
 			m_flLookTimeTotal	= -1;
+#ifdef NH2
+			if( HasSpawnFlags(SF_TRIGGERLOOK_INVERT) )
+			{
+				Trigger(pOther,false);
+			}
+#endif
 		}
 	}
 }
@@ -4623,7 +4639,7 @@ void CTriggerVPhysicsMotion::StartTouch( CBaseEntity *pOther )
 #ifndef _XBOX
 	if ( m_ParticleTrail.m_strMaterialName != NULL_STRING )
 	{
-		CEntityParticleTrail *pTrail = CEntityParticleTrail::Create( pOther, m_ParticleTrail, this ); 
+		CEntityParticleTrail::Create( pOther, m_ParticleTrail, this ); 
 		pTrail->SetShouldDeletedOnChangelevel( true );
 	}
 #endif
